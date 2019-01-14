9146bdd8f009f46d4097b589a2765ba3597c4d68
Demo96 <kubadem96@gmail.com>
2019-01-14 19:32:03 +0100

    filtrowanie listy uzytkownikow/userow

--- fe/aiapp/src/app/advertisments-list/advertisments-list.component.ts
+++ fe/aiapp/src/app/advertisments-list/advertisments-list.component.ts
@@ -3,6 +3,7 @@ import { Advertisment } from '../advertisment';
 import { Observable } from 'rxjs';
 import { AdvertismentService } from '../advertisment.service';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-advertisments-list',
@@ -11,6 +12,8 @@ import { Router } from '@angular/router';
 })
 export class AdvertismentsListComponent implements OnInit {
   advertisments: Observable<Advertisment[]>;
+  filteredAdvertisments: Observable<Advertisment[]>;
+  filter: string;
   constructor(private advertismentService: AdvertismentService, private router: Router) { }
 
   getShortDescription(advertisment: Advertisment): string {
@@ -20,23 +23,25 @@ export class AdvertismentsListComponent implements OnInit {
     else 
     return advertisment.description.slice(0,shortDescriptionMaxLength-3)+"...";
   }
+
   ngOnInit() {
     this.reloadData();
   }
+
   reloadData() {
-    this.advertisments = this.advertismentService.getAdvertismentList();
+   this.advertisments = this.advertismentService.getAdvertismentList();
+   this.filterAdvertisments();
   }
+
   deleteAdvertisment(id: number)
   {
     this.advertismentService.deleteAdvertisment(id).subscribe(() => this.reloadData());
   }
-  shouldBeShown(advertisment)
-  {
-    return true;
-   // return advertisment.title.includes('asd');
-  //  this.advertisments.forEach(function (advArray: Advertisment[]) {
-    //  console.log(advArray.filter(adv => adv.title.includes('asd')));
-   // this.advertisments=advArray.filter(adv => !adv.title.includes('asd'));
-  //  }); 
-  }
+
+  filterAdvertisments() {
+     if(this.filter) {
+       this.filteredAdvertisments=this.advertisments.pipe(map(advertisments => advertisments.filter(adv => adv.title.includes(this.filter))));
+     }
+     else this.filteredAdvertisments=this.advertisments;
+ }
 }
--- fe/aiapp/src/app/users-list/users-list.component.ts
+++ fe/aiapp/src/app/users-list/users-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../user';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users-list',
@@ -11,14 +12,26 @@ import { UserService } from '../user.service';
 })
 export class UsersListComponent implements OnInit {
   users: Observable<User[]>;
+  filteredUsers:  Observable<User[]>;
+  filter: string;
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.reloadData();
   }
+
   reloadData() {
     this.users = this.userService.getUsersList();
+    this.filterUsers();
+  }
+
+  filterUsers() {
+    console.log(this.filter);
+    if(this.filter)
+      this.filteredUsers=this.users.pipe(map(users => users.filter(user => user.userName.includes(this.filter))));
+    else
+     this.filteredUsers=this.users;
   }
 
 }
